50b593c4a4d642006badb516e8366246f423f906
shyam manek <51887543+shyammanek@users.noreply.github.com>
2021-09-12 15:54:02 +0530

    Update settings.py
    Edit your Email Configuration to Reset Email through password

--- crm1/settings.py
+++ crm1/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shyammanek953@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nikita$hyam004007'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Password'
 
 
 # Default primary key field type
